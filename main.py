f04a9d7b14d2a48b47ee0908b80d654d402b1241
ultrazi0 <vasiliy.lyadov@gmail.com>
2024-05-18 18:12:10 +0200

    fixed the issue that feedback was blocking closure

--- main.py
+++ main.py
@@ -61,6 +61,7 @@ if __name__ == "__main__":
     capture = Process(target=robot.cam.capture)
     send = Process(target=robot.ws_image.read_from_memory_and_send_windows)
     listen = Process(target=robot.ws_command.receive_commands_windows)
+    send_feedback = Process(target=robot.ws_feedback.connect_and_forward)
 
     GPIO.setmode(GPIO.BOARD)
 
@@ -88,6 +89,7 @@ if __name__ == "__main__":
         capture.start()
         send.start()
         listen.start()
+        send_feedback.start()
 
         while 1:
             message = robot.ws_command.receiver.recv()
@@ -104,6 +106,7 @@ if __name__ == "__main__":
     finally:
         robot.ws_image.closed.set()
         robot.ws_command.closed.set()
+        robot.ws_feedback.closed.set()
         robot.cam.image_memory.unlink()
 
         print("Main>>> THE END")
--- sock/FeedbackWebsocketClass.py
+++ sock/FeedbackWebsocketClass.py
@@ -1,12 +1,38 @@
 from websockets.sync.client import connect
 from sock.socketClass import Websocket
+from multiprocessing import Pipe
 
 
 class FeedbackWebsocket(Websocket):
 
     def __init__(self, uri: str):
         super().__init__(uri)
-        self.websocket = connect(self.uri)
+        # self.websocket = connect(self.uri)
+
+        self.receiver, self.sender = Pipe()
+    
+    @Websocket._connect_and_run
+    def connect_and_forward(self):
+        """
+        Connects to the websocket and waits for messages to forward them
+        """
+        try:
+            while 1:
+                message = self.receiver.recv()
+                self.websocket.send(message)
+
+                if self.closed.is_set():
+                    raise KeyboardInterrupt
+        except KeyboardInterrupt:
+            pass
+        finally:
+            print("FeedbackWebsocket>>> Closing...")
+
+            self.closed.set()
+    
+    def connect_and_forward_windows(self):
+        """ Same as FeedbackWebsocket.connect_and_forward() - workaround for Windows """
+        self.connect_and_forward()
 
     def send(self, message: str) -> None:
-        self.websocket.send(message)
+        self.sender.send(message)
